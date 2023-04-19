4ee5683dc57c4709000dbc6a60b7904cd01de193
rhuan22244 <rhuanbarrufi@gmail.com>
2023-04-19 20:32:56 -0300

    arquivo

---
+++ celsius e fahreinheint.py
@@ -0,0 +1,7 @@
+c=float(input("digite a temperatura em celsius:"))
+f=32+(9/5)*c
+k=c + 273
+
+print("a temperatura em celsius é:",c,"ºc")
+print("a temperatura em fahrenheit é:",f,"ºf")
+print("a temperatura em kelvin é:",k,"ºk")
